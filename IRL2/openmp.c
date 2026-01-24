d3ee73e842fcf7da8f9a9baa6f304903581e14fe
Simon NixOs Home <148442550+sisu6828@users.noreply.github.com>
2026-01-24 16:25:21 +0100

    added

---
+++ IRL2/openmp.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main () {
+    int N = 512 * 1024 * 1024;
+    int *A, *B;
+    long dot_product = 0;
+    double begin,end; // Timestamps for beginning and end of computation
+
+    //Allocate Array A and B with N elements
+    A = (int *) malloc (N * sizeof(int));
+    B = (int *) malloc (N * sizeof(int));
+    
+
+    //Initialize arrays
+    for (int i = 0; i < N; i++) {
+        A[i] = 1;
+        B[i] = 2;
+    }
+
+    //Now running time-stamp
+    begin = omp_get_wtime();
+    for (int i = 0; i < N; i++) {
+        dot_product += A[i] * B[i];
+    }
+    end = omp_get_wtime();
+
+    printf("The final dotproduct is %ld\n", dot_product);
+    printf("Time to execute was %lfs\n", end-begin);
+
+    return 0;
+}
